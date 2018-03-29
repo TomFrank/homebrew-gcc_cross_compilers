df4e4c99a2ab8b7365dcfa802784f019465d22f4
TomFrank <zzj97114@gmail.com>
2018-03-29 20:31:00 +0800

    gdb7.8 to gdb8.1 & gcc49 to gcc7

--- i386-elf-gdb.rb
+++ i386-elf-gdb.rb
@@ -2,17 +2,18 @@ require 'formula'
 
 class I386ElfGdb < Formula
   homepage 'http://www.gnu.org/software/gdb/'
-  url 'http://ftp.gnu.org/gnu/gdb/gdb-7.8.2.tar.xz'
-  sha256 '605954d5747d5f08ea4b7f48e958d1ebbf39265e18f7f36738deeabb83744485'
+  url "https://ftp.gnu.org/gnu/gdb/gdb-8.1.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gdb/gdb-8.1.tar.xz"
+  sha256 "af61a0263858e69c5dce51eab26662ff3d2ad9aa68da9583e8143b5426be4b34"
 
   depends_on 'i386-elf-binutils'
   depends_on 'i386-elf-gcc'
 
   def install
-    # ENV['CC'] = '/usr/local/bin/gcc-4.9'
-    # ENV['CXX'] = '/usr/local/bin/g++-4.9'
-    # ENV['CPP'] = '/usr/local/bin/cpp-4.9'
-    # ENV['LD'] = '/usr/local/bin/gcc-4.9'
+    # ENV['CC'] = '/usr/local/bin/gcc-7'
+    # ENV['CXX'] = '/usr/local/bin/g++-7'
+    # ENV['CPP'] = '/usr/local/bin/cpp-7'
+    # ENV['LD'] = '/usr/local/bin/gcc-7'
 
     mkdir 'build' do
       system '../configure', '--target=i386-elf', "--prefix=#{prefix}", '--disable-werror'
