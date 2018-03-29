de909a46977dad3ca3d7fcfc71002340038ef0b2
TomFrank <zzj97114@gmail.com>
2018-03-29 20:46:03 +0800

    upgrade binutils2.23 to 3.30

--- i386-elf-binutils.rb
+++ i386-elf-binutils.rb
@@ -2,8 +2,9 @@ require 'formula'
 
 class I386ElfBinutils < Formula
   homepage 'http://gcc.gnu.org'
-  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.23.tar.gz'
-  sha256 '7909a08eabdbaac0f7a22e9ede82a66ba70acd50629b045e705af864eef10b65'
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.30.tar.gz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.30.tar.gz"
+  sha256 "8c3850195d1c093d290a716e20ebcaa72eda32abf5e3d8611154b39cff79e9ea"
 
   depends_on 'gcc' => :build
 
--- x64-elf-binutils.rb
+++ x64-elf-binutils.rb
@@ -2,16 +2,17 @@ require 'formula'
 
 class X64ElfBinutils < Formula
   homepage 'http://gcc.gnu.org'
-  url 'http://ftp.gnu.org/gnu/binutils/binutils-2.23.tar.gz'
-  sha256 '7909a08eabdbaac0f7a22e9ede82a66ba70acd50629b045e705af864eef10b65'
+  url "https://ftp.gnu.org/gnu/binutils/binutils-2.30.tar.gz"
+  mirror "https://ftpmirror.gnu.org/binutils/binutils-2.30.tar.gz"
+  sha256 "8c3850195d1c093d290a716e20ebcaa72eda32abf5e3d8611154b39cff79e9ea"
 
-  depends_on 'gcc49' => :build
+  depends_on 'gcc' => :build
 
   def install
-    ENV['CC'] = '/usr/local/bin/gcc-4.9'
-    ENV['CXX'] = '/usr/local/bin/g++-4.9'
-    ENV['CPP'] = '/usr/local/bin/cpp-4.9'
-    ENV['LD'] = '/usr/local/bin/gcc-4.9'
+    ENV['CC'] = '/usr/local/bin/gcc-7'
+    ENV['CXX'] = '/usr/local/bin/g++-7'
+    ENV['CPP'] = '/usr/local/bin/cpp-7'
+    ENV['LD'] = '/usr/local/bin/gcc-7'
 
     mkdir 'build' do
       system '../configure', '--disable-nls', '--target=x86_64-elf','--disable-werror',
